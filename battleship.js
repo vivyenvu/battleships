354f26be697ce4fced564a53e7f4ed72440aa570
Vivyen Vu <pokeviv@gmail.com>
2022-06-02 05:34:59 -0400

    fix typo in prompt user

--- battleship.js
+++ battleship.js
@@ -9,7 +9,7 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false) {
-    guess = promt("Ready, aim, fire! (enter a number from 0-6):");
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
     if (guess < 0 || guess > 6){
         alert("Please enter a valid cell number!");
     }
